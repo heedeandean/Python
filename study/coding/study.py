db392e7e623127b8dc61a98258d57f2fa926e049
heedeandean <7942ahj@hanmail.net>
2019-07-18 21:52:32 +0900

    2019-07-18

--- study/coding/study.py
+++ study/coding/study.py
@@ -1,6 +1,8 @@
-def cnt(list):
-    if list == []:
-        return 0
-    return 1 + cnt(list[1:])
+def max(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
 
-print(cnt([1,2,3,4,5,6,7,8,9]))
+    next_max = max(list[1:])
+    return list[0] if list[0] > next_max else next_max
+
+print(max([1, 5, 8, 25, 100, 20]))
