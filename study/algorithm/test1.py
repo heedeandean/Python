78ca69550528f8776b4821cb270686ac7659c232
heedeandean <7942ahj@hanmail.net>
2019-07-22 23:46:27 +0900

    2019-07-22

--- study/algorithm/test1.py
+++ study/algorithm/test1.py
@@ -13,11 +13,14 @@ def count_self(list):
     return 1 + count_self(list[1:])       
 
 # 3. 리스트에서 가장 큰 수를 찾는 max_self()
-
-
+def max_self(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
+    sub_max = max_self(list[1:])
+    return list[0] if list[0] > sub_max else sub_max
 
 # 출력
 list = [4, 7, 10]
 print("sum : ", sum_self(list))
 print("count : ", count_self(list))
-# print("max : ", max_self(list))
+print("max : ", max_self(list))
