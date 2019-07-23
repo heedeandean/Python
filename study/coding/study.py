f5142131fcfb2d67c85cfe963c6dc2ef877b1f51
heedeandean <7942ahj@hanmail.net>
2019-07-24 00:00:48 +0900

    2019-07-24

--- study/coding/study.py
+++ study/coding/study.py
@@ -1,29 +1,55 @@
-from collections import deque
+graph= {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
 
-def person_is_seller(name):
-    return name[-1] == 'm'
+graph["a"] = {}
+graph["a"]["fin"] = 1
 
-graph = {}
-graph["희진"] = ["영신", "유엘", "유나m"]
-graph["영신"] = []
-graph["유엘"] = []
-graph["유나"] = []
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
 
-def search(name):
-    search_queue = deque()
-    search_queue += graph[name]
+graph["fin"] = {}
 
-    searched = []
+# 가격
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
 
-    while search_queue:
-        person = search_queue.popleft()
-        if not person in searched:
-            if person_is_seller(person):
-                print(person + "은 망고 판매상!ㅎㅎ")
-                return True
-            else:
-                search_queue += graph[person]
-                searched.append(person)
-    return False
 
-search("희진")
+# 부모
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+print(costs)
