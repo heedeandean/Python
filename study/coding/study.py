24497885d41f984579ac02e7a802d092816b993d
heedeandean <7942ahj@hanmail.net>
2019-09-14 00:50:59 +0900

    2019-09-14

--- study/coding/study.py
+++ study/coding/study.py
@@ -1,9 +1,8 @@
-def trans(c):
-    return c*9/5+32
-
-
-def trans_lam(c): return c*9/5+32
-
-
-for t in (22.6, 25.8, 27.3, 29.8):
-    print(t, "C : ", trans_lam(t), "F")
+class MYSU:
+    def __init__(self, Su):
+        self.Su=Su
+    def __lt__(self, Su):
+        print("self .Su < other.Su")
+    def __gt__(self, Su):
+        print("self .Su > other.Su")
+    
