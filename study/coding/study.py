586d1288845d002ea4f922e11fe22b9417e71eff
heedeandean <7942ahj@hanmail.net>
2019-09-15 16:03:17 +0900

    2019-09-15

--- study/coding/study.py
+++ study/coding/study.py
@@ -1,8 +1,20 @@
-class MYSU:
-    def __init__(self, Su):
-        self.Su=Su
-    def __lt__(self, Su):
-        print("self .Su < other.Su")
-    def __gt__(self, Su):
-        print("self .Su > other.Su")
+class Doit:
+    def __init__(self, temp = 0):
+        self._temp = temp
     
+    def to_value(self):
+        return (self.temp * 1.8) + 32
+
+    @property
+    def temp(self):
+        return self._temp
+
+    @temp.setter
+    def temp(self, value):
+        self._temp = value
+
+v = Doit()
+
+for i in range(10, 15):
+    v.temp = i
+    print(str(v.to_value()))
