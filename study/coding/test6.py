a919dd33c5823af4ba58b7ac07182cbfd68d880a
heedeandean <7942ahj@hanmail.net>
2019-09-25 20:26:40 +0900

    2019-09-25

--- study/coding/test6.py
+++ study/coding/test6.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     csv_file = open('fruit.csv', 'w')
     cw = csv.writer(csv_file, delimiter=',', quotechar='|')
     rows = [["사과", 3000, 3000, 5000]
-            , ["오렌지", 7000, 2000, 1000]
-            , ["바나나", 2000, 6000, 7000]]
+           , ["오렌지", 7000, 2000, 1000]
+           , ["바나나", 2000, 6000, 7000]]
 
     cw.writerows(rows)
     csv_file.close()
