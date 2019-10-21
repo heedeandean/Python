bfa7317c8c4c18eab4ed9b247c84e5ceaeb75cb6
heedeandean <7942ahj@hanmail.net>
2019-10-21 21:25:45 +0900

    2019-10-21

--- study/coding/study.py
+++ study/coding/study.py
@@ -1,6 +1,24 @@
 # Hint. findSmallest와 selectionSort 메서드를 만들것.
 
 
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+
+    for i in range(1, len(arr)):
+        if smallest > arr[i]:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+
+    for i in range(len(arr)):
+        smallest_index = findSmallest(arr)
+        newArr.append(arr.pop(smallest_index))
+    return newArr
 
 
 print(selectionSort([5, 3, 6, 2, 10]))
