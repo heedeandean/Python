ab46c7c254e02077833087a80bc933148a8d2103
heedeandean <7942ahj@hanmail.net>
2019-10-22 21:40:13 +0900

    2019-10-22

--- study/coding/study.py
+++ study/coding/study.py
@@ -1,14 +1,25 @@
 # 재귀함수 이용!
 
 # 1. sum()
-
+def sum(arr):
+    if arr == []:
+        return 0
+    return arr[0] + sum(arr[1:])
 
 # 2. count()
+def count(arr):
+    if arr == []:
+        return 0
+    return 1 + count(arr[1:])
 
 
 # 3. max()
-
-
+def max(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    else:
+        sub_max = max(arr[1:])
+        return arr[0] if arr[0] > sub_max else sub_max
 
 
 # 출력
