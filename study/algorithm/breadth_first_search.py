e6e8f8ea6c4f2a738d2bebcba1dc2982b76211a0
heedeandean <7942ahj@hanmail.net>
2019-10-27 17:01:13 +0900

    2019-10-27

--- study/algorithm/breadth_first_search.py
+++ study/algorithm/breadth_first_search.py
@@ -1,7 +1,6 @@
 # 너비 우선 탐색
 # 문제 : 망고상인(m으로 끝나는 이름) 찾기
 
-
 graph = {}
 graph["you"] = ["alice", "bob", "claire"]
 graph["bob"] = ["anuj", "peggy"]
@@ -13,5 +12,8 @@ graph["thom"] = []
 graph["jonny"] = []
 
 # (Hint. person_is_seller, search 메서드 만들기)
+
+
+
 
 search("you")
