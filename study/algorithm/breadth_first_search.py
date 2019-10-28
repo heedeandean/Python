b7084603cd0994073a9340e1edcf2e0b336e1392
heedeandean <7942ahj@hanmail.net>
2019-10-28 21:08:35 +0900

    2019-10-28

--- study/algorithm/breadth_first_search.py
+++ study/algorithm/breadth_first_search.py
@@ -1,6 +1,8 @@
 # 너비 우선 탐색
 # 문제 : 망고상인(m으로 끝나는 이름) 찾기
 
+from collections import deque
+
 graph = {}
 graph["you"] = ["alice", "bob", "claire"]
 graph["bob"] = ["anuj", "peggy"]
@@ -11,9 +13,26 @@ graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
 
-# (Hint. person_is_seller, search 메서드 만들기)
+
+def person_is_seller(person):
+    return person[-1] == 'm'
 
 
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + "이 망고상인!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
 
 
 search("you")
