301ee336ec50224a760b3d40a106d4bed6e1c25e
heedeandean <7942ahj@hanmail.net>
2021-01-06 00:44:26 +0900

    2021-01-06

---
+++ Flask/minitwit/schema.sql
@@ -0,0 +1,18 @@
+create table if not exists user (
+    user_id integer primary key autoincrement,
+    username string not null,
+    email string not null,
+    pw_hash string not null
+);
+
+create table if not exists follower (
+    who_id integer,
+    whom_id integer
+);
+
+create table if not exists message (
+    message_id integer primary key autoincrement,
+    author_id integer not null,
+    text string not null,
+    pub_date integer
+);
